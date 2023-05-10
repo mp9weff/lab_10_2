efa4fc6dfbde7164edb32381381f739f36498a17
unknown <mariapusahuk4@gmail.com>
2023-05-10 18:36:07 +0300

    lab_10_2

---
+++ Person.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_10_2
+{
+    [Serializable]
+    public class Person
+    {
+        private string name;
+        private DateTime birthYear;
+
+        //властивості
+        public string Name
+        {
+            get { return name; }
+        }
+        public DateTime BirthYear
+        {
+            get { return birthYear; }
+        }
+
+        //констркутор за замовчуванням та з параметрами
+        public Person()
+        {
+
+        }
+        public Person(string name, DateTime birthYear)
+        {
+            this.name = name;
+            this.birthYear = birthYear;
+        }
+
+        //методи
+        public int Age()
+        {
+            int age = DateTime.Now.Year - birthYear.Year;
+            return (age);
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Enter name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter birth year: ");
+            birthYear = System.Convert.ToDateTime(Console.ReadLine());
+        }
+
+        //Зміна імені
+        public void ChangeName(string newName)
+        {
+            name = newName;
+        }
+
+        //Метод ToString() повертає рядок з інформацією про ім'я, рік народження та вік людини.
+        public override string ToString()
+        {
+            return $"Name: {name}, Birth Year: {birthYear.ToString("yyyy")}, Age: {Age()}";
+        }
+
+        //Порівняння на ім'я
+        public void OutPut()
+        {
+            Console.WriteLine("Information about this people: ");
+            Console.WriteLine(ToString());
+        }
+
+        public static bool operator ==(Person person1, Person person2)
+        {
+            return (person1.name == person2.name);
+        }
+
+        public static bool operator !=(Person person1, Person person2)
+        {
+            return !(person1 == person2);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace lab_10_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // створити новий об'єкт Person
+            Person person = new Person("John Doe", new DateTime(1990, 5, 10));
+
+            // серіалізуємо об'єкт у файл
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Person));
+            using (FileStream stream = File.Create("person.json"))
+            {
+                serializer.WriteObject(stream, person);
+            }
+
+            // десеріалізація об'єкта з файлу
+            using (FileStream stream = File.OpenRead("person.json"))
+            {
+                Person deserializedPerson = (Person)serializer.ReadObject(stream);
+                deserializedPerson.OutPut(); // друкуємо інформацію про десеріалізовану особу
+            }
+        }
+    }
+}
+
